c62ca35e42623dae6c42e44d1b63f9257e86a663
RuiTakao <ruia1082halfnc@gmail.com>
2023-02-04 05:53:59 +0900

    directry change

---
+++ dist/template/template.php
@@ -0,0 +1,9 @@
+<?php
+require_once('dist/template/src/name-entry-section.html');
+require_once('dist/template/src/playing-card-game-section.html');
+require_once('dist/template/src/opening-message-label.html');
+require_once('dist/template/src/playing-card-game-section__card.html');
+require_once('dist/template/src/truth-or-callenge.html');
+require_once('dist/template/src/choice-truth-or-callenge.html');
+require_once('dist/template/src/order-truth-content.html');
+require_once('dist/template/src/order-challenge-content.html');
--- dist/truth-or-dare.php
+++ dist/truth-or-dare.php
@@ -1,5 +1,18 @@
-<?php
-require_once('./layouts/header.php');
-require_once('./template/template.php');
-echo '<div id="app" class="container"></div>';
-require_once('./layouts/footer.php');
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="dist/app.css" />
+    <script type="module" src="dist/app.js" defer></script>
+    <title>真実か挑戦かゲーム</title>
+</head>
+
+<body>
+    <?php require_once('dist/template/template.php') ?>
+    <div id="app" class="container"></div>
+</body>
+
+</html>
